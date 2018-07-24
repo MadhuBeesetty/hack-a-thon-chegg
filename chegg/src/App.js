979e7895773abfdaf8ded54ef974116951df4878
madhusudhanrao beesetty <madhusudhan@madhusudhan-2.local>
2018-07-24 14:15:56 -0700

    final css touch for questions and answers

--- chegg/src/App.js
+++ chegg/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Router, NavLink, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 import Main from './components/Main';
 import CheggStudyPractice from './components/CheggStudyPractice';
@@ -11,11 +11,11 @@ class App extends Component {
   constructor() {
     super();
     this.state={
-      questionsAll: [ { questionNumber: 0, question: 'madhu', answer: 'answer1' },
-        { questionNumber: 1, question: 'sudhan', answer: 'answer2' },
-        { questionNumber: 2, question: 'beesetty', answer: 'answer3' },
-        { questionNumber: 3, question: 'extraQuestion', answer: 'answer4' },
-        { questionNumber: 4, question: 'extraQuestion1', answer: 'answer5' }],
+      questionsAll: [ { questionNumber: 1, question: 'madhu', answer: 'answer1' },
+        { questionNumber: 2, question: 'sudhan', answer: 'answer2' },
+        { questionNumber: 3, question: 'beesetty', answer: 'answer3' },
+        { questionNumber: 4, question: 'extraQuestion', answer: 'answer4' },
+        { questionNumber: 5, question: 'extraQuestion1', answer: 'answer5' }],
     }
   }
 
--- chegg/src/components/AnswersAndMore.js
+++ chegg/src/components/AnswersAndMore.js
@@ -7,33 +7,33 @@ class AnswersAndMore extends Component {
     return (
       <div>
         <img className="pic1" src={require('./AnswersTop.png')} alt="answertop" />
-        <div class="dropdown">
-  <button class="dropbtn">Thomas'Calculus(14th Ed.)</button>
-  <div class="dropdown-content">
-    <a href="#">Link 1</a>
-    <a href="#">Link 2</a>
-    <a href="#">Link 3</a>
-  </div>
-</div>
-        <div>
-          {this.props.QuestionsAll.map((each, index) => {
-            return(
-              <div className="marginBorder" key={index} >
-                <p className="question" >Problem {each.questionNumber}:</p>
-                <details>
-                  <summary>question:{each.question}</summary>
-                  <p className="background" >answer:{each.answer}</p>
-                </details>
-              </div>
-            )
-          })
-          }
-          <NavLink to='CheggStudyPractice'>
-            <input className="button" type="submit" value="CheggStudyPractice" />
-          </NavLink>
-        </div>
-        <img className="pic2" src={require('./AnswersDown.png')} alt="answerdown" />
+        <div className="dropdown">
+          <button className="dropbtn">Thomas'Calculus(14th Ed.)</button>
+          <div className="dropdown-content">
+            <a href="#">Link 1</a>
+            <a href="#">Link 2</a>
+            <a href="#">Link 3</a>
+          </div>
       </div>
+      <div>
+        {this.props.QuestionsAll.map((each, index) => {
+          return(
+            <div className="marginBorder" key={index} >
+              <p className="question" >Problem {each.questionNumber}:</p>
+              <details>
+                <summary className="answer" >question:{each.question}</summary>
+                <p className="background" >answer:{each.answer}</p>
+              </details>
+            </div>
+          )
+        })
+        }
+        <NavLink to='CheggStudyPractice'>
+          <input className="button" type="submit" value="CheggStudyPractice" />
+        </NavLink>
+      </div>
+      <img className="pic2" src={require('./AnswersDown.png')} alt="answerdown" />
+    </div>
     );
   }
 }
